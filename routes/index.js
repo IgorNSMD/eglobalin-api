0cff90eb41ef46e3bb99832c0b9fedce673c38f6
IgorNSMD <insanmartind@gmail.com>
2023-06-01 16:16:22 -0400

    P2

--- routes/index.js
+++ routes/index.js
@@ -6,31 +6,31 @@ const categoriaController = require('../controllers/categoriasController')
 module.exports = function(){
 
     router.get('/', (req,res)=> {
-        res.send('inicio eglobalin-ism..P1')
+        res.send('inicio eglobalin-ism..P2')
     })
 
     // CategoriaS
 
-    // Agrega nueva Categoria via POST
-    router.post('/categorias', 
-    categoriaController.nuevaCategoria)
+    // // Agrega nueva Categoria via POST
+    // router.post('/categorias', 
+    // categoriaController.nuevaCategoria)
 
-    // Obtener metodo GET
-    router.get('/categorias', 
-    categoriaController.mostrarCategorias)    
+    // // Obtener metodo GET
+    // router.get('/categorias', 
+    // categoriaController.mostrarCategorias)    
 
-    // Muestra Categoria en especifico
-    router.get('/categorias/:idCategoria', 
-    categoriaController.mostrarCategoria)    
+    // // Muestra Categoria en especifico
+    // router.get('/categorias/:idCategoria', 
+    // categoriaController.mostrarCategoria)    
 
-    // Actualizar Categoria
-    router.put('/categorias/:idCategoria', 
-    categoriaController.actualizarCategoria)    
+    // // Actualizar Categoria
+    // router.put('/categorias/:idCategoria', 
+    // categoriaController.actualizarCategoria)    
 
 
-    // Eliminar Categoria
-    router.delete('/categorias/:idCategoria', 
-    categoriaController.eliminarCategoria)        
+    // // Eliminar Categoria
+    // router.delete('/categorias/:idCategoria', 
+    // categoriaController.eliminarCategoria)        
 
     
     return router
